474d744964af88eaec50c253dba081a66e6cae26
agostino <agostinocuomo9@gmail.com>
2024-06-24 22:21:05 +0200

    esercizio 18/ pulsante modifica non funziona

---
+++ app/Livewire/Page/Modifica.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire\Page;
+
+use Livewire\Component;
+use Livewire\Attributes\Validate;
+
+
+class Modifica extends Component
+{
+
+   
+
+    #[Validate('required|string')]
+    public $name;
+   #[Validate('required|string')]
+   public $gender; 
+
+   public $article;
+
+   public function mount()
+    {
+        $this->name = $this->article->name;
+        $this->gender = $this->article->gender;
+       
+    }
+
+   public function update()
+   {
+       $this->validate();
+
+       $this->article->update([
+           'name' => $this->name,
+           'gender' => $this->gender,
+          
+       ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.page.modifica');
+    }
+}
